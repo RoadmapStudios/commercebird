437cf97caf987ae36211172c8fbff803f15a24e4
Fawad Tiemoerie <info@roadmapstudios.com>
2022-08-26 13:54:50 +0200

    finalized the cost price api

--- inventory-for-woocommerce.php
+++ inventory-for-woocommerce.php
@@ -91,33 +91,57 @@ function wooventory_cors( $allowed_origins ) {
   }
 add_filter('allowed_http_origins', 'wooventory_cors', 10, 1);
 
-// The code for creating Product Cost Price in WooCommerce as Meta
-add_action( 'woocommerce_product_options_general_product_data', 'wooventory_cost_price' ); 
-add_action( 'woocommerce_process_product_meta', 'wooventory_cost_price_save' );
 
-function wooventory_cost_price()
-{
-    global $woocommerce, $post;
-    echo '<div class="product_custom_field">';
-    // Custom Product Text Field
-    woocommerce_wp_text_input(
-        array(
-            'id' => 'cost_price',
-            'placeholder' => 'Cost Price',
-            'label' => __('Cost Price', 'woocommerce'),
-            'desc_tip' => 'true'
-        )
-    );
-    echo '</div>';
-}
+// Only enable cost_price if ATUM plugin is not installed
+// if (! defined( 'ATUM_VERSION' ) ) {
+	// The code for creating Product Cost Price in WooCommerce as Meta
+	add_action( 'woocommerce_product_options_general_product_data', 'wooventory_cost_price' ); 
+	add_action( 'woocommerce_process_product_meta', 'wooventory_cost_price_save' );
 
-function wooventory_cost_price_save($post_id)
-{
-    // Custom Product Text Field
-    $woocommerce_custom_product_text_field = $_POST['cost_price'];
-    if (!empty($woocommerce_custom_product_text_field))
-        update_post_meta($post_id, 'cost_price', esc_attr($woocommerce_custom_product_text_field));
-}
+	function wooventory_cost_price()
+	{
+		global $woocommerce, $post;
+		echo '<div class="product_custom_field">';
+		// Custom Product Text Field
+		woocommerce_wp_text_input(
+			array(
+				'id' => 'cost_price',
+				'placeholder' => 'Cost Price',
+				'label' => __('Cost Price', 'woocommerce'),
+				'desc_tip' => 'true'
+			)
+		);
+		echo '</div>';
+	}
+
+	function wooventory_cost_price_save($post_id)
+	{
+		// Custom Product Text Field
+		$woocommerce_custom_product_text_field = $_POST['cost_price'];
+		if (!empty($woocommerce_custom_product_text_field))
+			update_post_meta($post_id, 'cost_price', esc_attr($woocommerce_custom_product_text_field));
+	}
+
+	// Release cost price in the Woo API
+	add_action( 'rest_api_init', 'wooventory_register_post_meta' );
+	function wooventory_register_post_meta() {
+		register_rest_field( 'product', // any post type registered with API
+			'cost_price', // this needs to match meta key
+			array(
+				'get_callback' => 'wooventory_get_meta',
+				'update_callback' => 'wooventory_update_meta',
+				'schema' => null,
+			)
+		);
+	}
+	function wooventory_get_meta( $object, $field_name, $request ) {
+		return get_post_meta( $object[ 'id' ], $field_name, true );
+	}
+	function wooventory_update_meta( $value, $object, $field_name ) {
+		return update_post_meta( $object->id, $field_name, $value );
+	}
+
+// } // end of Atum plugin check
 
 // function wooventory_client_activate( $slash = '' ) {
 //     $config = file_get_contents (ABSPATH . "wp-config.php");
