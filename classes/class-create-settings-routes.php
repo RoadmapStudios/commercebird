1d100a270956c14ad7622e7e3b986eb85c28f91a
Fawad Tiemoerie <info@roadmapstudios.com>
2023-03-30 23:29:32 +0200

    updated CORS in PHP

--- classes/class-create-settings-routes.php
+++ classes/class-create-settings-routes.php
@@ -10,100 +10,103 @@ $woocommerce = new Client(
     'ck_b0305b88f6d5d26e6423c073f6f95de119cc9e55',
     'cs_609c13f281816b937499b2a0052e1145745acdc3',
     [
-      'version' => 'wc/v3',
+        'version' => 'wc/v3',
     ]
-  );
+);
 
-class WP_React_Settings_Rest_Route {
+class WP_React_Settings_Rest_Route
+{
 
     protected $woocomerce;
-    public function __construct($woo) {
+    public function __construct($woo)
+    {
         $this->woocomerce = $woo;
-        add_action( 'rest_api_init', [ $this, 'create_rest_routes' ] );
+        add_action('rest_api_init', [$this, 'create_rest_routes']);
     }
 
-    public function create_rest_routes() {
-        register_rest_route( 'wooventory/v1', '/settings', [
+    public function create_rest_routes()
+    {
+        register_rest_route('wooventory/v1', '/settings', [
             'methods' => 'GET',
-            'callback' => [ $this, 'get_settings' ],
-            'permission_callback' => [ $this, 'get_settings_permission' ]
-        ] );
-        register_rest_route( 'wooventory/v1', '/settings', [
+            'callback' => [$this, 'get_settings'],
+            'permission_callback' => [$this, 'get_settings_permission'],
+        ]);
+        register_rest_route('wooventory/v1', '/settings', [
             'methods' => 'POST',
-            'callback' => [ $this, 'save_settings' ],
-            'permission_callback' => [ $this, 'save_settings_permission' ]
-        ] );
+            'callback' => [$this, 'save_settings'],
+            'permission_callback' => [$this, 'save_settings_permission'],
+        ]);
 
-        register_rest_route( 'wooventory/v1', '/subscription/(?P<id>\d+)', [
+        register_rest_route('wooventory/v1', '/subscription/(?P<id>\d+)', [
             'methods' => 'GET',
-            'callback' => [ $this, 'get_subscription' ],
-            'permission_callback' => [ $this, 'get_settings_permission' ]
-        ] );
+            'callback' => [$this, 'get_subscription'],
+            'permission_callback' => [$this, 'get_settings_permission'],
+        ]);
     }
 
-
-    public function get_subscription($data){
+    public function get_subscription($data)
+    {
         $sub_id = $data['id'];
         try {
             $endpoint = 'subscriptions/' . $sub_id;
-            
+
             // logging starts here
-            $fd = fopen(__DIR__.'/get_subscription.txt','w+');
+            $fd = fopen(__DIR__ . '/get_subscription.txt', 'w+');
             $response = $this->woocomerce->get($endpoint);
-            fwrite($fd, PHP_EOL. print_r(array($response,$woocommerce), true));
+            fwrite($fd, PHP_EOL . print_r(array($response, $woocommerce), true));
             fclose($fd);
             return $response;
         } catch (HttpClientException $e) {
-            $fd = fopen(__DIR__.'/get_subscription.txt','w+');
-            fwrite($fd, PHP_EOL. print_r($e, true));
+            $fd = fopen(__DIR__ . '/get_subscription.txt', 'w+');
+            fwrite($fd, PHP_EOL . print_r($e, true));
             fclose($fd);
             return false;
-            
+
         }
     }
 
-    public function get_settings() {
-        $response =[
-		    "sub_id" => get_option("wooventory_sub_id"),
-            "cors_status" => get_option("enable_corse") 
-		];
-        return rest_ensure_response( $response );
+    public function get_settings()
+    {
+        $response = [
+            "sub_id" => get_option("wooventory_sub_id"),
+            "cors_status" => get_option("enable_corse"),
+        ];
+        return rest_ensure_response($response);
     }
 
-    public function get_settings_permission() {
+    public function get_settings_permission()
+    {
         return true;
     }
 
-    public function save_settings( $req ) {
+    public function save_settings($req)
+    {
         //enable corse
-        if($req["cors_status"] == true) {
-            echo '<pre>';
-            print_r($req);die();
-            $fp = fopen('.htaccess','a+');
-            if($fp){
-                fwrite($fp,'
-                <IfModule mod_headers.c>
-                    Header set Access-Control-Allow-Origin "*"
-                </IfModule>');
-                fclose($fp);
-            }
-            update_option("enable_corse",true);
+        if ($req["cors_status"] == true) {
+            // Get path to main .htaccess for WordPress
+            $htaccess = get_home_path() . ".htaccess";
+
+            $lines = array();
+            $lines[] = "<IfModule mod_headers.c> Header set Access-Control-Allow-Origin "*" </IfModule>";
+            insert_with_markers($htaccess, "CORS Support", $lines);
+            
+            update_option("enable_corse", true);
         }
 
-
-        if(!empty($req["sub_id"])) {
+        if (!empty($req["sub_id"])) {
             $sub_id = $req["sub_id"];
             $res = $this->get_subscription(array("sub_id" => $sub_id));
-            if($res == false){
+            if ($res == false) {
                 return rest_ensure_response("failure");
             }
             update_option('wooventory_sub_id', $sub_id);
         }
 
-        return rest_ensure_response( 'success' );
+        return rest_ensure_response('success');
     }
 
-    public function save_settings_permission() {
+    public function save_settings_permission()
+    {
         return true;
     }
 }
