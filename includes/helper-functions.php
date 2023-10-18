521241946446d92b094f189a3eb66230c7184bcc
Roadmap Studios Zoho Partner <info@roadmapstudios.com>
2023-10-18 14:47:00 +0200

    replaced order_timestamp function with clear product cache

--- includes/helper-functions.php
+++ includes/helper-functions.php
@@ -4,23 +4,11 @@
  * Helper functions to ensure correct handling of Data being transferred via rest api
  */
 
-function wooventory_correct_order_timestamp($object, $request, $is_creating)
+function wooventory_clear_product_cache($object, $request, $is_creating)
 {
-    if ($is_creating) {
-        // Get the current date and time in the required format
-        $current_datetime = current_time('Y-m-d H:i:s');
-
-        $order_id = $object->get_id();
-
-        // Update the post_date and post_date_gmt
-        $order_data = array(
-            'ID' => $order_id,
-            'post_date' => $current_datetime,
-            'post_date_gmt' => get_gmt_from_date($current_datetime),
-        );
-
-        // Update the order post
-        wp_update_post($order_data);
+    if (!$is_creating) {
+        $product_id = $object->get_id();
+        wc_delete_product_transients($product_id);
     }
 }
-add_action('woocommerce_rest_insert_shop_order_object', 'wooventory_correct_order_timestamp', 10, 3);
+add_action('woocommerce_rest_insert_product_object', 'wooventory_clear_product_cache', 10, 3);
