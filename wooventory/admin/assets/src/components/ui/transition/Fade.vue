12d34c9d3034eb86a565f870a9ea9b61b07ca3e0
Dev Kabir <dev.kabir01@gmail.com>
2023-11-10 08:42:00 +0600

    deleted build version

--- wooventory/admin/assets/src/components/ui/transition/Fade.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <transition enter-active-class="duration-500 ease-in-out bg-black" enter-from-class="opacity-0"
-              enter-to-class="opacity-25" leave-active-class="duration-500 ease-in-out bg-black"
-              leave-from-class="opacity-25"
-              leave-to-class="opacity-0">
-    <slot/>
-  </transition>
-</template>
-  
