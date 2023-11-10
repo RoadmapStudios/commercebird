ac3a4bb90cf5d3fc4e552174c3f11c8610e6cdb7
Dev Kabir <dev.kabir01@gmail.com>
2023-11-10 08:42:00 +0600

    deleted build version

--- wooventory/admin/assets/src/components/ui/BaseLink.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <a class="flex min-w-fit items-center gap-2 px-4 py-2 font-medium bg-white border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2"
-     v-bind="$attrs">
-    <slot/>
-  </a>
-</template>
-  
--- wooventory/admin/assets/src/components/ui/transition/Slide.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <transition enter-active-class="transition duration-500 ease-in-out transform sm:duration-700"
-              enter-from-class="translate-x-full" enter-to-class="translate-x-0"
-              leave-active-class="transition duration-500 ease-in-out transform sm:duration-700"
-              leave-from-class="translate-x-0"
-              leave-to-class="translate-x-full">
-    <slot/>
-  </transition>
-</template>
