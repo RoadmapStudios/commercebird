21142a484aedf38bb2833dcfdcdb4b28fcfb8bd3
Dev Kabir <dev.kabir01@gmail.com>
2023-11-23 19:35:19 +0600

    bug fix

--- admin/includes/Template.php
+++ admin/includes/Template.php
@@ -78,7 +78,7 @@ final class Template {
 				'security_token' => wp_create_nonce( self::NAME ),
 				'redirect_uri'   => admin_url( 'admin.php?page=wooventory-app' ),
 				'url'            => admin_url( 'admin-ajax.php' ),
-				'wc_tax_enabled' => wc_tax_enabled(),
+				'wc_tax_enabled' => is_plugin_active('woocommerce/woocommerce.php') ? wc_tax_enabled() : false,
 				'roles'          => $wp_roles->get_names(),
 				'b2b_enabled'    => class_exists( 'Addify_B2B_Plugin' ),
 			),
