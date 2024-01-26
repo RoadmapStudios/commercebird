980445614d087d37a809ceadfafd90bfb8c775ed
Fawad Tiemoerie <info@roadmapstudios.com>
2024-01-26 17:17:31 +0100

    Update Field.vue

--- admin/assets/src/components/forms/zoho/Field.vue
+++ admin/assets/src/components/forms/zoho/Field.vue
@@ -1,15 +1,12 @@
 <template>
   <div>
-    <div
-        v-for="(field, index) in store.fields"
-        :key="index"
-        class="grid items-end grid-cols-5 gap-4"
-    >
+    <h1 class="pt-4 pb-2 text-xl font-bold tracking-tight border-b">Orders Custom Fields</h1>
+    <div v-for="(field, index) in store.fields" :key="index" class="grid items-end gap-4 sm:grid-cols-5">
       <InputGroup label="WooCommerce Field" type="repeater">
-        <SelectInput v-model="field.key" :options="store.customFields"/>
+        <SelectInput v-model="field.key" :options="store.customFields" />
       </InputGroup>
       <InputGroup label="Zoho Field Label" type="repeater">
-        <TextInput v-model="field.value"/>
+        <TextInput v-model="field.value" />
       </InputGroup>
       <div class="pb-[11px]">
         <BaseButton @click="store.removeField(index)">Remove</BaseButton>
@@ -18,17 +15,12 @@
     <div class="flex justify-between gap-4 mt-8">
       <BaseButton @click="store.addField()">Add Item</BaseButton>
       <div class="flex gap-4">
-        <BaseButton
-            :loading="loader.isLoading(backendAction.save_fields)"
-            @click="store.handleSubmit(backendAction.save_fields)"
-        >
+        <BaseButton :loading="loader.isLoading(backendAction.zohoInventory.field.save)"
+          @click="store.handleSubmit(backendAction.zohoInventory.field.save)">
           Save
         </BaseButton>
-        <BaseButton
-            :loading="loader.isLoading(backendAction.reset_fields)"
-            type="lite"
-            @click="store.handleReset(backendAction.reset_fields)"
-        >
+        <BaseButton :loading="loader.isLoading(backendAction.zohoInventory.field.reset)" type="lite"
+          @click="store.handleReset(backendAction.zohoInventory.field.reset)">
           Reset
         </BaseButton>
       </div>
