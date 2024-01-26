4bc5f1f0e014dbea367b5305b60035a4e757d23c
Dev Kabir <dev.kabir01@gmail.com>
2024-01-27 01:40:31 +0600

    form action fixe

--- admin/assets/src/components/forms/zoho/Field.vue
+++ admin/assets/src/components/forms/zoho/Field.vue
@@ -1,31 +1,21 @@
 <template>
-  <div>
-    <h1 class="pt-4 pb-2 text-xl font-bold tracking-tight border-b">Orders Custom Fields</h1>
+  <BaseForm :keys="action" @reset="store.handleReset(action.reset)" @submit="store.handleSubmit(action.save)">
+    <div class="pt-4 pb-2 flex justify-between items-center   tracking-tight border-b">
+      <h1 class="text-xl font-bold">Orders Custom Fields</h1>
+      <BaseButton @click="store.addField()">Add Item</BaseButton>
+    </div>
     <div v-for="(field, index) in store.fields" :key="index" class="grid items-end gap-4 sm:grid-cols-5">
       <InputGroup label="WooCommerce Field" type="repeater">
-        <SelectInput v-model="field.key" :options="store.customFields" />
+        <SelectInput v-model="field.key" :options="store.customFields"/>
       </InputGroup>
       <InputGroup label="Zoho Field Label" type="repeater">
-        <TextInput v-model="field.value" />
+        <TextInput v-model="field.value"/>
       </InputGroup>
       <div class="pb-[11px]">
         <BaseButton @click="store.removeField(index)">Remove</BaseButton>
       </div>
     </div>
-    <div class="flex justify-between gap-4 mt-8">
-      <BaseButton @click="store.addField()">Add Item</BaseButton>
-      <div class="flex gap-4">
-        <BaseButton :loading="loader.isLoading(backendAction.zohoInventory.field.save)"
-          @click="store.handleSubmit(backendAction.zohoInventory.field.save)">
-          Save
-        </BaseButton>
-        <BaseButton :loading="loader.isLoading(backendAction.zohoInventory.field.reset)" type="lite"
-          @click="store.handleReset(backendAction.zohoInventory.field.reset)">
-          Reset
-        </BaseButton>
-      </div>
-    </div>
-  </div>
+  </BaseForm>
 </template>
 
 <script lang="ts" setup>
@@ -36,7 +26,9 @@ import InputGroup from "../../ui/inputs/InputGroup.vue";
 import TextInput from "../../ui/inputs/TextInput.vue";
 import BaseButton from "../../ui/BaseButton.vue";
 import SelectInput from "../../ui/inputs/SelectInput.vue";
+import BaseForm from "@/components/ui/BaseForm.vue";
 
+const action = backendAction.zohoInventory.field;
 const store = useZohoInventoryStore();
 const loader = useLoadingStore();
 </script>
--- admin/assets/src/components/forms/zoho/Order.vue
+++ admin/assets/src/components/forms/zoho/Order.vue
@@ -1,51 +1,43 @@
 <template>
-  <div>
+  <BaseForm :keys="action" @reset="store.handleReset(action.reset)" @submit="store.handleSubmit(action.save)">
     <div class="grid grid-cols-2 gap-4 my-4">
       <InputGroup label="Create Package" type="toggle">
-        <Toggle v-model="store.order_settings.package_sync"/>
+        <Toggle v-model="store.order_settings.package_sync" />
       </InputGroup>
       <InputGroup label="Disable order sync" type="toggle">
-        <Toggle v-model="store.order_settings.disable_sync"/>
+        <Toggle v-model="store.order_settings.disable_sync" />
       </InputGroup>
       <InputGroup label="Enable auto sales order number" type="toggle">
-        <Toggle v-model="store.order_settings.enable_auto_number"/>
+        <Toggle v-model="store.order_settings.enable_auto_number" />
       </InputGroup>
       <InputGroup label="Send all Orders as Draft" type="toggle">
-        <Toggle v-model="store.order_settings.enable_order_status"/>
+        <Toggle v-model="store.order_settings.enable_order_status" />
       </InputGroup>
     </div>
     <InputGroup v-if="store.order_settings.enable_auto_number" label="Order Prefix">
-      <TextInput v-model="store.order_settings.order_prefix" placeholder="Set Order Prefix like WC"/>
+      <TextInput v-model="store.order_settings.order_prefix" placeholder="Set Order Prefix like WC" />
     </InputGroup>
     <InputGroup label="Select a Warehouse (optional)">
-      <SelectInput v-model="store.order_settings.warehouse_id" :options="store.zoho_warehouses"/>
+      <SelectInput v-model="store.order_settings.warehouse_id" :options="store.zoho_warehouses" />
     </InputGroup>
     <div v-if="store.order_settings.warehouse_id" class="flex items-center py-4 gap-x-6">
       <label>Use this Warehouse also for Stock Sync</label>
-      <Toggle v-model="store.order_settings.enable_warehousestock"/>
+      <Toggle v-model="store.order_settings.enable_warehousestock" />
     </div>
-    <div class="flex gap-4 mt-8">
-      <BaseButton :loading="loader.isLoading(backendAction.save_order)" @click="store.handleSubmit"
-                  @click.prevent="store.handleSubmit(backendAction.save_order)">
-        Save
-      </BaseButton>
-      <BaseButton :loading="loader.isLoading(backendAction.reset_order)" type="lite"
-                  @click.prevent="store.handleReset(backendAction.reset_order)">Reset
-      </BaseButton>
-    </div>
-  </div>
+  </BaseForm>
 </template>
 
 <script lang="ts" setup>
-import {backendAction} from '@/keys';
-import {useLoadingStore} from '@/stores/loading';
-import {useZohoInventoryStore} from '@/stores/zohoInventory';
+import { backendAction } from '@/keys';
+import { useLoadingStore } from '@/stores/loading';
+import { useZohoInventoryStore } from '@/stores/zohoInventory';
 import InputGroup from '../../ui/inputs/InputGroup.vue';
 import Toggle from '../../ui/inputs/Toggle.vue';
 import TextInput from '../../ui/inputs/TextInput.vue';
 import SelectInput from '../../ui/inputs/SelectInput.vue';
-import BaseButton from '../../ui/BaseButton.vue';
+import BaseForm from "@/components/ui/BaseForm.vue";
 
+const action = backendAction.zohoInventory.order;
 const store = useZohoInventoryStore();
 const loader = useLoadingStore();
 </script>
