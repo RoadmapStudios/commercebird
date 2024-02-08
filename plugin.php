4fe83bcc2fa1edda44bc51fea70ed90e4e2eb321
Fawad Tiemoerie <info@roadmapstudios.com>
2024-02-08 18:13:14 +0100

    bumped version

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name: CommerceBird
  * Plugin URI:  https://commercebird.com
  * Description: This plugin helps you get the most of CommerceBird by allowing you to upload product images, use integrations like Zoho CRM & Exact Online and more.
- * Version: 2.1.1
+ * Version: 2.1.2
  * Author: CommerceBird
  * Author URI:  https://commercebird.com
  * Requires PHP: 7.4
@@ -29,7 +29,7 @@ if ( ! defined( 'RMS_PLUGIN_NAME' ) ) {
 	define( 'RMS_PLUGIN_NAME', 'CommerceBird' );
 }
 if ( ! defined( 'RMS_VERSION' ) ) {
-	define( 'RMS_VERSION', '2.1.1' );
+	define( 'RMS_VERSION', '2.1.2' );
 }
 if ( ! defined( 'RMS_DIR_PATH' ) ) {
 	define( 'RMS_DIR_PATH', plugin_dir_path( __FILE__ ) );
