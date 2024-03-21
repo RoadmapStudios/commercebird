9775676de8daa16ef20a53f3303bf26177117d9b
kcss-ashvini <ashvini.p@kcssinfotech.com>
2024-03-21 16:59:21 +0530

    updated zoho-crm page for fields tab

--- admin/assets/src/page/ZohoCRM.vue
+++ admin/assets/src/page/ZohoCRM.vue
@@ -1,20 +1,22 @@
 <script lang="ts" setup>
-import { CurrencyDollarIcon, LinkIcon, ShoppingBagIcon, UsersIcon, TruckIcon } from "@heroicons/vue/24/outline";
+import { LinkIcon, TruckIcon,TableCellsIcon } from "@heroicons/vue/24/outline";
 import { onBeforeMount } from "vue";
-import { useExactOnlineStore } from "@/stores/exactOnline";
 import TabComponent from "@/components/ui/tabs/TabComponent.vue";
 import RequiredNotice from "@/components/ui/RequiredNotice.vue";
 
 import { acf_enabled } from "@/composable/helpers";
 import Connect from "@/components/forms/zoho-crm/Connect.vue";
 import Order from "@/components/forms/zoho-crm/Order.vue";
+import Field from "@/components/forms/zoho-crm/Field.vue";
+import { useZohoCrmStore } from "@/stores/zohoCrm";
 
-const store = useExactOnlineStore()
+const store = useZohoCrmStore();
 
 
 const tabs = {
     connect: { title: "Connect", component: Connect, icon: LinkIcon },
     order: { title: "Orders", component: Order, icon: TruckIcon },
+    field: { title: "Custom Fields", component: Field, icon: TableCellsIcon },
 };
 
 
