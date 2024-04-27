75f9a76736bda564c36f26b46cb618688ca94b40
Roadmap Studios Zoho Partner <info@roadmapstudios.com>
2024-04-27 11:07:12 +0200

    Update build-mac.sh

--- build-mac.sh
+++ build-mac.sh
@@ -2,6 +2,7 @@
 PLUGIN_SLUG="$(basename $PWD)"
 PROJECT_PATH=$(pwd)
 BUILD_PATH="${PROJECT_PATH}/build"
+DEST_PATH="$BUILD_PATH/$PLUGIN_SLUG"
 
 # Function to display progress messages
 progress_message() {
@@ -21,7 +22,8 @@ set -e
 # prepare place for build.
 progress_message "Preparing build directory..."
 rm -rf "$BUILD_PATH"
-mkdir -p "$BUILD_PATH"
+rm -rf "$PLUGIN_SLUG".zip
+mkdir -p "$DEST_PATH"
 
 # build assets
 progress_message "Building admin template..."
@@ -29,31 +31,27 @@ npm --prefix ./admin/assets/ run build-only
 
 # copy all files for production
 progress_message "Copying files for production..."
-rsync -rc --exclude-from="$PROJECT_PATH/.distignore" "$PROJECT_PATH/" "$BUILD_PATH/" --delete --delete-excluded
-rsync -rc "$PROJECT_PATH/admin/assets/dist" "$BUILD_PATH/admin/assets"
+rsync -rc --exclude-from="$PROJECT_PATH/.distignore" "$PROJECT_PATH/" "$DEST_PATH/" --delete --delete-excluded
+rsync -rc "$PROJECT_PATH/admin/assets/dist" "$DEST_PATH/admin/assets"
 
 ## Install PHP dependencies
 progress_message "Installing PHP dependencies..."
-composer install --working-dir="$BUILD_PATH" --no-dev
-rm "$BUILD_PATH"/composer.lock
+composer install --working-dir="$DEST_PATH" --no-dev
+rm "$DEST_PATH"/composer.lock
 
 progress_message "Removing dev data..."
-sed -i '' '66,69d' "$BUILD_PATH"/admin/includes/Template.php
+sed -i '' '66,69d' "$DEST_PATH"/admin/includes/Template.php
 ## Add index.php to every directory
 progress_message "Adding index.php to every directory..."
-find "$BUILD_PATH" -type d -exec sh -c "echo '<?php // silence' > {}/index.php" \;
+find "$DEST_PATH" -type d -exec sh -c "echo '<?php // silence' > {}/index.php" \;
 
 ## Create zip archive
 progress_message "Creating zip archive..."
 cd "$BUILD_PATH" || exit
-zip -r "${PLUGIN_SLUG}.zip" "$BUILD_PATH/"
+zip -q -r "${PLUGIN_SLUG}.zip" "$PLUGIN_SLUG/"
 
 cd "$PROJECT_PATH" || exit
-mv "build/${PLUGIN_SLUG}.zip" "$PROJECT_PATH"
-
-## Revert changes for production
-#progress_message "Reverting changes..."
-# rm -rf ./"$PLUGIN_SLUG"
+mv "$BUILD_PATH/${PLUGIN_SLUG}.zip" "$PROJECT_PATH"
 
 # Completion message
 progress_message "Build process completed successfully."
