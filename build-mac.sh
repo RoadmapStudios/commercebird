648acb57b6045cefacdc86e5cfbd80a32eb7a3f9
Roadmap Studios Zoho Partner <info@roadmapstudios.com>
2024-04-27 09:54:38 +0200

    fixed build script for Mac

--- build-mac.sh
+++ build-mac.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
+PLUGIN_SLUG="$(basename $PWD)"
+PROJECT_PATH=$(pwd)
+BUILD_PATH="${PROJECT_PATH}/build"
 
 # Function to display progress messages
 progress_message() {
@@ -16,10 +19,9 @@ progress_message() {
 set -e
 
 # prepare place for build.
-plugin_name="$(basename $PWD)"
 progress_message "Preparing build directory..."
-rm -rf ./"$plugin_name" ./"$plugin_name".zip
-mkdir ./"$plugin_name"
+rm -rf "$BUILD_PATH"
+mkdir -p "$BUILD_PATH"
 
 # build assets
 progress_message "Building admin template..."
@@ -27,31 +29,31 @@ npm --prefix ./admin/assets/ run build-only
 
 # copy all files for production
 progress_message "Copying files for production..."
-rsync -a --exclude "$plugin_name" --exclude "$plugin_name".zip --exclude node_modules --exclude vendor --exclude src --exclude .git --exclude .gitignore --exclude .DS_Store --exclude build.sh . ./"$plugin_name"/
+rsync -rc --exclude-from="$PROJECT_PATH/.distignore" "$PROJECT_PATH/" "$BUILD_PATH/" --delete --delete-excluded
+rsync -rc "$PROJECT_PATH/admin/assets/dist" "$BUILD_PATH/admin/assets"
 
-# cp -R  ./includes ./lib ./libraries ./admin/assets/dist ./admin/includes ./vendor ./*.php composer.json changelog.txt  LICENSE README.md  ./"$plugin_name"/ --parents
-
-#
 ## Install PHP dependencies
 progress_message "Installing PHP dependencies..."
-composer install --working-dir=./"$plugin_name" --no-dev
-rm ./"$plugin_name"/composer.json
-rm ./"$plugin_name"/composer.lock
+composer install --working-dir="$BUILD_PATH" --no-dev
+rm "$BUILD_PATH"/composer.lock
 
 progress_message "Removing dev data..."
-sed -i '71,78d' ./"$plugin_name"/plugin.php
-sed -i '67,70d' ./"$plugin_name"/admin/includes/Template.php
+sed -i '' '66,69d' "$BUILD_PATH"/admin/includes/Template.php
 ## Add index.php to every directory
 progress_message "Adding index.php to every directory..."
-find ./"$plugin_name" -type d -exec sh -c "echo '<?php // silence' > {}/index.php" \;
+find "$BUILD_PATH" -type d -exec sh -c "echo '<?php // silence' > {}/index.php" \;
 
 ## Create zip archive
 progress_message "Creating zip archive..."
-zip -r ./"$plugin_name".zip ./"$plugin_name"/*
+cd "$BUILD_PATH" || exit
+zip -r "${PLUGIN_SLUG}.zip" "$BUILD_PATH/"
+
+cd "$PROJECT_PATH" || exit
+mv "build/${PLUGIN_SLUG}.zip" "$PROJECT_PATH"
 
 ## Revert changes for production
 #progress_message "Reverting changes..."
-# rm -rf ./"$plugin_name"
+# rm -rf ./"$PLUGIN_SLUG"
 
 # Completion message
 progress_message "Build process completed successfully."
