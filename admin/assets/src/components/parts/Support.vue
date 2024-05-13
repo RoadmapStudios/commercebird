c4a50aeb4ebd7e789cddb261205fb063d937878e
Roadmap Studios Zoho Partner <info@roadmapstudios.com>
2024-05-13 09:22:13 +0200

    Update Support.vue

--- admin/assets/src/components/parts/Support.vue
+++ admin/assets/src/components/parts/Support.vue
@@ -37,7 +37,7 @@ const links = {
   meeting: {
     title: "Schedule a Meeting",
     subtitle: "Book a Demo with our Sales Team",
-    link: "https://book.commercebird.com",
+    link: "https://book.commercebird.com/#/customer/roadmapstudios",
     icon: CalendarDaysIcon,
     button: "Book Now",
   },
