8f29f276db9efa5bda5f5999075b93d52e72844e
Manish Chaubey <developer.mkc@gmail.com>
2024-06-11 18:07:25 +0530

    View component updated

---
+++ admin/assets/src/components/forms/exact-online/Webhooks.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="grid grid-cols-2 gap-4 my-4">
+    <BaseForm
+      :keys="action"
+      @reset="store.handleReset(action.reset)"
+      @submit="store.handleSubmit(action.save)"
+    >
+      <div>
+        <InputGroup label="SalesInvoices Webhook" type="toggle">
+          <Toggle v-model="store.webhook_settings.enable_SalesInvoices" />
+        </InputGroup>
+        <InputGroup label="StockPosition Webhook" type="toggle">
+          <Toggle v-model="store.webhook_settings.enable_StockPostion" />
+        </InputGroup>
+        <InputGroup label="Item Webhook" type="toggle">
+          <Toggle v-model="store.webhook_settings.enable_Item" />
+        </InputGroup>
+      </div>
+    </BaseForm>
+
+    <div>
+      <!-- <InputGroup>
+        <TextInput></TextInput>
+        </InputGroup>
+        <InputGroup>
+        <TextInput></TextInput>
+        </InputGroup>
+       <InputGroup>
+        <TextInput></TextInput>
+        </InputGroup> -->
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import BaseForm from "@/components/ui/BaseForm.vue";
+import TextInput from "@/components/ui/inputs/TextInput.vue";
+import InputGroup from "@/components/ui/inputs/InputGroup.vue";
+import Toggle from "@/components/ui/inputs/Toggle.vue";
+import { useExactOnlineStore } from "@/stores/exactOnline";
+import { backendAction } from "@/keys";
+const store = useExactOnlineStore();
+const action = backendAction.exactOnline.webhooks;
+</script>
