8486a4f0de3a5efbea751da7936edb9bb7e51343
Roadmap Studios Zoho Partner <info@roadmapstudios.com>
2024-06-24 12:09:12 +0200

    Update class-api-for-exact-webhooks.php

--- includes/classes/apis/class-api-for-exact-webhooks.php
+++ includes/classes/apis/class-api-for-exact-webhooks.php
@@ -10,7 +10,7 @@ defined( 'RMS_PLUGIN_NAME' ) || exit();
 
 class Exact extends WP_REST_Controller {
 
-	protected $prefix = 'v2';
+	protected $prefix = 'wc/v3';
 	protected $rest_base = 'exact-webhooks';
 
 	public function __construct() {
@@ -21,9 +21,19 @@ class Exact extends WP_REST_Controller {
 			array(
 				'methods' => WP_REST_Server::CREATABLE,
 				'callback' => array( $this, 'process_webhook' ),
-				'permission_callback' => '__return_true',
+				'permission_callback' => array( $this, 'permission_check' ),
 			)
 		);
+	}
+
+	/**
+	 * Check if a given request has access to get items.
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
+	 * @return WP_Error|bool
+	 */
+	public function permission_check( $request ) {
+		return current_user_can( 'manage_woocommerce' );
 	}
 
 	public function process_webhook( $request ) {
@@ -63,7 +73,7 @@ class Exact extends WP_REST_Controller {
 					$product->save();
 				} else {
 					// if product not found, create a new product if the $data['data']['Webshop'] is set to true
-					if ( ! $data['data']['Webshop'] ) {
+					if ( ! $data['data']['IsWebshopItem'] ) {
 						break;
 					}
 					$product = new WC_Product();
@@ -80,6 +90,28 @@ class Exact extends WP_REST_Controller {
 				break;
 			case 'StockPositions':
 				// process StockPositions webhook
+				// get the "ItemId" from $data['data']['ItemId'] and find the product based on product meta key "eo_item_id"
+				$item_id = $data['data']['ItemId'];
+				// find the product id based on meta key "eo_item_id" which has the value of $item_id using WP query
+				$args = array(
+					'post_type' => 'product',
+					'meta_query' => array(
+						array(
+							'key' => 'eo_item_id',
+							'value' => $item_id,
+						),
+					),
+				);
+				$query = new WP_Query( $args );
+				if ( $query->have_posts() ) {
+					while ( $query->have_posts() ) {
+						$query->the_post();
+						$product_id = get_the_ID();
+						$product = wc_get_product( $product_id );
+						$product->set_stock_quantity( $data['data']['FreeStock'] );
+						$product->save();
+					}
+				}
 				break;
 			default:
 				// process default webhook
