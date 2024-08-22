28d8b2601a3ca0a2b566c961ea9cb3c03c29d7ee
Roadmap Studios Zoho Partner <info@roadmapstudios.com>
2024-08-22 23:20:50 +0200

    bumped version

--- commercebird.php
+++ commercebird.php
@@ -4,7 +4,7 @@
  * Plugin URI:  https://commercebird.com
  * Author:      CommerceBird
  * Description: This plugin helps you get the most of CommerceBird by allowing you to upload product images, use integrations like Zoho CRM & Exact Online and more.
- * Version: 2.1.26
+ * Version: 2.2.0
  * Requires PHP: 7.4
  * Requires Plugins: woocommerce
  * Requires at least: 6.5
@@ -19,7 +19,7 @@
  * @license   https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0-or-later
  *
  * WC requires at least: 8.0.0
- * WC tested up to: 9.1.2
+ * WC tested up to: 9.2.2
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -29,7 +29,7 @@ if ( ! defined( 'RMS_PLUGIN_NAME' ) ) {
 	define( 'RMS_PLUGIN_NAME', 'CommerceBird' );
 }
 if ( ! defined( 'RMS_VERSION' ) ) {
-	define( 'RMS_VERSION', '2.1.26' );
+	define( 'RMS_VERSION', '2.2.0' );
 }
 if ( ! defined( 'RMS_DIR_PATH' ) ) {
 	define( 'RMS_DIR_PATH', plugin_dir_path( __FILE__ ) );
