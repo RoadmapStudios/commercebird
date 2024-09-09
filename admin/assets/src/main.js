63b5bad197f67cc2085e3deda666818f45af20ed
Roadmap Studios Zoho Partner <info@roadmapstudios.com>
2024-09-09 17:30:04 +0200

    updated sweetalert

--- admin/assets/src/main.js
+++ admin/assets/src/main.js
@@ -5,9 +5,14 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "@/route";
+
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(VueSweetalert2);
 app.use(router);
 
 app.mount("#commercebird-app");
