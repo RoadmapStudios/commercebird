0beda4eda43ef7e5abde7d072011a5d3f306089d
Fawad Tiemoerie <info@roadmapstudios.com>
2025-02-04 19:28:46 +0100

    added ubuntu build script

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -7,64 +7,47 @@ DEST_PATH="$BUILD_PATH/$PLUGIN_SLUG"
 # Function to display progress messages
 progress_message() {
   local message="$1"
-
-  # Define color codes
-  local color_reset="\033[0m"
   local color_green="\033[32m"
-
-  # Print the colorized message
+  local color_reset="\033[0m"
   echo -e "[$(date +'%Y-%m-%d %H:%M:%S')] ${color_green}${message}${color_reset}"
 }
 
-# abort on errors
+# Abort on errors
 set -e
 
-# prepare place for build.
+# Prepare build directory
 progress_message "Preparing build directory..."
 rm -rf "$BUILD_PATH"
 rm -rf "$PLUGIN_SLUG".zip
 mkdir -p "$DEST_PATH"
 
-# build assets
+# Install npm dependencies and build assets
 progress_message "Building admin template..."
+npm --prefix ./admin/assets/ ci
 npm --prefix ./admin/assets/ run build-only
 
-# copy all files for production
+# Copy all files for production
 progress_message "Copying files for production..."
 rsync -rc --exclude-from="$PROJECT_PATH/.distignore" "$PROJECT_PATH/" "$DEST_PATH/" --delete --delete-excluded
 rsync -rc "$PROJECT_PATH/admin/assets/dist" "$DEST_PATH/admin/assets"
 
-# Modify `index.js` to remove lines with URLs ending in .mp3
+# Modify `index.js` to remove .mp3 URLs (Linux-compatible sed)
 INDEX_JS_PATH="$DEST_PATH/admin/assets/dist/index.js"
-
 if [ -f "$INDEX_JS_PATH" ]; then
-    progress_message "Modifying index.js to remove lines with URLs ending in .mp3..."
-
-    # Use sed to remove lines containing .mp3 URLs (macOS-compatible)
-    sed -i '' 's/n\.src="https:\/\/[^"]*\.mp3"/n.src=""/g' "$DEST_PATH/admin/assets/dist/index.js"
-    # Optionally, verify the modification
-    if grep -q '\.mp3' "$INDEX_JS_PATH"; then
-        echo "Failed to remove lines with .mp3 URLs."
-        exit 1
-    else
-        echo "Lines with .mp3 URLs successfully removed."
-    fi
-else
-    echo "index.js not found. Skipping modification."
+    progress_message "Modifying index.js to remove lines with .mp3 URLs..."
+    sed -i 's/n\.src="https:\/\/[^"]*\.mp3"/n.src=""/g' "$INDEX_JS_PATH"
 fi
 
-## Install PHP dependencies
+# Install PHP dependencies
 progress_message "Installing PHP dependencies..."
 composer install --working-dir="$DEST_PATH" --no-dev
-rm "$DEST_PATH"/composer.lock
+rm "$DEST_PATH/composer.lock"
 
-progress_message "Removing dev data..."
-sed -i '' '74,77d' "$DEST_PATH"/admin/includes/Template.php
-## Add index.php to every directory
+# Add index.php to every directory
 progress_message "Adding index.php to every directory..."
 find "$DEST_PATH" -type d -exec sh -c "echo '<?php // silence' > {}/index.php" \;
 
-## Create zip archive
+# Create zip archive
 progress_message "Creating zip archive..."
 cd "$BUILD_PATH" || exit
 zip -q -r "${PLUGIN_SLUG}.zip" "$PLUGIN_SLUG/"
