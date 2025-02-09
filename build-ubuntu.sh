b0363688f0fb1ca85d3b6dfbf78c1b6b9688e50d
Fawad Tiemoerie <info@roadmapstudios.com>
2025-02-09 20:45:35 +0100

    Update build-ubuntu.sh

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -38,6 +38,8 @@ mkdir -p "$DEST_PATH/admin/assets/dist"
 
 progress_message "Copying files for production..."
 rsync -av --exclude-from="$PROJECT_PATH/.distignore" "$PROJECT_PATH/" "$DEST_PATH/" --delete
+# copy admin/assets/dist files to build directory from project directory
+rsync -av "$PROJECT_PATH/admin/assets/dist/" "$DEST_PATH/admin/assets/dist"
 
 # copy composer.json to build directory from project directory
 rsync -rc "$PROJECT_PATH/composer.json" "$DEST_PATH/composer.json"
