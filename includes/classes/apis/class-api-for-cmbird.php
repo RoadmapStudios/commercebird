e37f9df4531758b28beaaad9b4131eb9a92ea651
Fawad Tiemoerie <info@roadmapstudios.com>
2025-02-20 18:49:41 +0100

    cleaned cmbird api unused code

--- includes/classes/apis/class-api-for-cmbird.php
+++ includes/classes/apis/class-api-for-cmbird.php
@@ -12,19 +12,12 @@ use WP_REST_Controller;
 
 class CMBird_APIs extends WP_REST_Controller {
 
+	use Api;
+	
 	protected $prefix = 'wc/v3';
 	protected $rest_base = 'cmbird';
 
 	public function __construct() {
-		// register_rest_route(
-		// 	$this->prefix,
-		// 	'/' . $this->rest_base . '/invoice-detail/',
-		// 	array(
-		// 		'methods' => WP_REST_Server::CREATABLE,
-		// 		'callback' => array( $this, 'get_zi_invoice_detail' ),
-		// 		'permission_callback' => array( $this, 'permission_check' ),
-		// 	)
-		// );
 		register_rest_route(
 			$this->prefix,
 			'/' . $this->rest_base . '/products-skus/',
@@ -63,55 +56,5 @@ class CMBird_APIs extends WP_REST_Controller {
 		$rest_response->set_data( $results );
 
 		return rest_ensure_response( $rest_response );
-
-		// connection
-		// $zoho_inventory_oid = get_option( 'cmbird_zoho_inventory_oid' );
-		// $zoho_inventory_url = get_option( 'cmbird_zoho_inventory_url' );
-		// $get_url = $zoho_inventory_url . 'inventory/v1/organizations/' . $zoho_inventory_oid . '?organization_id=' . $zoho_inventory_oid;
-
-		// $execute_curl_call_handle = new CMBIRD_API_Handler_Zoho();
-		// $json = $execute_curl_call_handle->execute_curl_call_get( $get_url );
-		// $code = $json->code;
-		// if ( 0 === (int) $code ) {
-		// 	$response['zi_org_name'] = $json->organization->name;
-		// 	$response['zi_org_email'] = $json->organization->email;
-		// 	$response['zi_org_id'] = $zoho_inventory_oid;
-		// 	$response['zi_api_url'] = $zoho_inventory_url;
-
-		// 	$rest_response->set_data( $response );
-		// 	$rest_response->set_status( 200 );
-
-		// } else {
-		// 	$rest_response->set_data( 'connection is not yet setup' );
-		// 	$rest_response->set_status( 400 );
-		// }
-
-		// return rest_ensure_response( $rest_response );
-	}
-
-	public function handle_get_api_request( $get_url, $data_key, $response_key ): WP_REST_RESPONSE {
-		$rest_response = new WP_REST_Response();
-		$rest_response->set_data( $this->empty_response );
-		$rest_response->set_status( 400 );
-		$execute_curl_call_handle = new CMBIRD_API_Handler_Zoho();
-		$json = $execute_curl_call_handle->execute_curl_call_get( $get_url );
-		$code = $json->code;
-		if ( 0 === (int) $code ) {
-			$response[ $response_key ] = $json->$data_key;
-			$response['url'] = $get_url;
-			$rest_response->set_data( $response );
-			$rest_response->set_status( 200 );
-		} else {
-			$rest_response->set_data( $json );
-			$rest_response->set_status( 400 );
-		}
-		return rest_ensure_response( $rest_response );
-	}
-
-	public function get_zi_invoices(): WP_REST_Response {
-		$zoho_inventory_oid = get_option( 'cmbird_zoho_inventory_oid' );
-		$zoho_inventory_url = get_option( 'cmbird_zoho_inventory_url' );
-		$get_url = $zoho_inventory_url . "inventory/v1/invoices/?organization_id=$zoho_inventory_oid";
-		return $this->handle_get_api_request( $get_url, 'invoices', 'invoices' );
 	}
 }
