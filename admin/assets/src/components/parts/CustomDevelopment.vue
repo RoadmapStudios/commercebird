4917e9492c1cbee268247a111d455d767a37efad
Fawad Tiemoerie <info@roadmapstudios.com>
2025-03-18 07:18:04 +0100

    added custom dev card

---
+++ admin/assets/src/components/parts/CustomDevelopment.vue
@@ -0,0 +1,10 @@
+<template>
+      <Card title="Hire us for Custom Development">
+        <DotLottieVue style="height: 500px; width: 500px" autoplay loop src="https://lottie.host/embed/0dba548d-a751-48ce-9119-507f5eb26974/jX3ZS24AWl.lottie" />
+  </Card>
+</template>
+
+<script lang="ts" setup>
+import Card from "@/components/ui/Card.vue";
+import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
+</script>
