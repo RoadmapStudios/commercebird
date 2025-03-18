556697f7de2cfe0955e17648bb16744422d7e7c0
Manish Chaubey <developer.mkc@gmail.com>
2025-03-18 14:58:45 +0530

    home component updated

--- admin/assets/src/page/HomePage.vue
+++ admin/assets/src/page/HomePage.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="grid gap-4 lg:grid-cols-2">
-    <Settings/>
-    <Subscription/>
-    <AppLogin/>
-    <Support/>
-    <CustomDevelopment/>
+    <Settings />
+    <Subscription />
+    <CustomDevelopment />
+    <div class="gap-4 px-2">
+      <AppLogin />
+      <Support />
+    </div>
+    <!-- Next component here -->
   </div>
 </template>
 <script lang="ts" setup>
@@ -13,10 +16,9 @@ import Settings from "@/components/forms/Settings.vue";
 import AppLogin from "@/components/parts/AppLogin.vue";
 import Support from "@/components/parts/Support.vue";
 import CustomDevelopment from "@/components/parts/CustomDevelopment.vue";
-import {onMounted} from "vue";
-import {useHomepageStore} from "@/stores/homepage";
+import { onMounted } from "vue";
+import { useHomepageStore } from "@/stores/homepage";
 
 const store = useHomepageStore();
 onMounted(store.load);
-
 </script>
